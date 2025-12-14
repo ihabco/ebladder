7f61a8031240687c9f352e44a08fd40d8ee7fa73
Ihab <ihabhani@live.com>
2025-12-14 19:34:10 +0200

    modified names and changed the icons

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:retinco/main.dart';
+import 'package:ebladder/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
